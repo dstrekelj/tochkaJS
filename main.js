a6097414d4b22ad15186098714e29d09db4e0287
dstrekelj <domagojstrekelj@gmail.com>
2014-09-24 19:05:25 +0200

    Initial commit.

---
+++ main.js
@@ -0,0 +1,104 @@
+var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'game');
+
+var mainState = {
+	/**
+	 * Preload game assets.
+	 */
+	preload: function() {
+		game.stage.backgroundColor = '#333333';
+		/**
+		 * Phaser does not support geometry for sprites, so
+		 * use graphics instead.
+		 */
+		game.load.image('player', 'assets/player.png');
+		game.load.image('obstacle', 'assets/obstacle.png');
+		/**
+		 * Necessary for displaying FPS.
+		 */
+		game.time.advancedTiming = true;
+    },
+	
+	/**
+	 * Set up the game.
+	 */
+    create:	function() {
+		/**
+		 * Enable (arcade) physics, making extra calculations
+		 * unnecessary.
+		 */
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		/**
+		 * Define player (sprite, gravity and hitbox).
+		 */
+		this.player = this.game.add.sprite(100, this.game.world.centerY - 20, 'player');
+		game.physics.arcade.enable(this.player);
+		this.player.body.gravity.y = 1400;
+		this.player.body.setSize(30, 30, 5, 5);
+		/**
+		 * Define obstacle (group, sprite, spawn timer).
+		 */
+		this.obstacles = game.add.group();
+		this.obstacles.enableBody = true;
+		this.obstacles.createMultiple(15 , 'obstacle');
+		this.scoreTimer = game.time.events.loop(1000, this.addScore, this);
+		/**
+		 * Define score label (score, text, increment timer).
+		 */
+		this.score = 0;
+		this.labelScore = game.add.text(20, 20, "0", {font: "16px Arial", fill: "#ffffff"});
+		this.timer = game.time.events.loop(300, this.addObstacle, this);
+		/**
+		 * Define controls.
+		 */
+		var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		spaceKey.onDown.add(this.jump, this);
+    },
+
+	/**
+	 * Update game state.
+	 */
+    update:	function() {
+		if (this.player.inWorld == false) {
+			this.retry();
+		}
+		
+		game.physics.arcade.overlap(this.player, this.obstacles, this.retry, null, this);
+    },
+	
+	jump: function() {
+		this.player.body.velocity.y = -450;
+	},
+	
+	retry: function() {
+		game.state.start('main');
+	},
+	
+	addObstacle: function() {
+		var obstacle = this.obstacles.getFirstDead();
+		
+		obstacle.body.setSize(30, 30, 5, 5);
+		
+		obstacle.reset(this.game.world.width, (this.game.world.height - 40) * game.rnd.frac());
+		
+		obstacle.body.velocity.x = -400 * ( 1 + game.rnd.frac());
+		
+		obstacle.checkWorldBounds = true;
+		obstacle.outOfBoundsKill = true;
+	},
+	
+	addScore: function() {
+		this.score += 1;
+		this.labelScore.text = this.score;
+	},
+	
+	render: function()
+	{
+		game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
+	}
+};
+
+/**
+ * Add mainState as main game state and starting point
+ */
+game.state.add('main', mainState);
+game.state.start('main');
